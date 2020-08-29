00832851b9918d528a02d7d65677c8304c616f4c
ankitaargade <argade06ankita@gmail.com>
2020-08-29 14:36:33 +0530

    first commit of counter-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = "Counter-App";
+  count: number = 0;
+  isCountNegative: boolean = false;
+  onIncrement() {
+    this.count = this.count + 1;
+    this.isCountNegative = this.isNegative();
+    console.log(this.isCountNegative);
+  }
+
+  onDecrement() {
+    this.count = this.count - 1;
+    this.isCountNegative = this.isNegative();
+    console.log(this.isCountNegative);
+  }
+  onReset() {
+    this.count = 0;
+    this.isCountNegative = this.isNegative();
+    console.log(this.isCountNegative);
+  }
+
+  isNegative() {
+    if (this.count >= 0) return false;
+    else return true;
+  }
 }
